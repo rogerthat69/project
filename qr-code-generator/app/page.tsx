e34d8b6f347ce4f5ae4d37d3af426d374cb40d55
rogerthat69 <rogeramir443@gmail.com>
2024-12-03 19:47:15 +0500

    Add files via upload

---
+++ qr-code-generator/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import QrCodeGenerator from "./QrCodeGenerator";
+
+export default function Home() {
+  return (
+    <div className="relative min-h-[100vh] h-full flex justify-center items-center">
+      <QrCodeGenerator />
+      <Image
+        src="/glass.png"
+        width={1600}
+        height={1200}
+        alt="glass image"
+        className="fixed top-0 left-0 w-full h-full object-cover z-0 pointer-events-none"
+      />
+    </div>
+  );
+}
